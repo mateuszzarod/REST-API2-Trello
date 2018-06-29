39bf52bb6a7289de084b0d9753629fb3bdc6cbe7
mateuszzarod <mateusz.zarod@gmail.com>
2018-06-30 00:07:33 +0200

    initial commi2t

---
+++ src/main/java/com/crud/tasks2/Tasks2Application.java
@@ -0,0 +1,16 @@
+package com.crud.tasks2;
+
+import com.crud.tasks2.domain.TaskDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tasks2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tasks2Application.class, args);
+
+
+
+	}
+}
